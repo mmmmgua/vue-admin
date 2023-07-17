241f44620583629f16a3cf8aaf0936453f515cab
mmmmgua <542126316@qq.com>
2023-07-17 17:06:11 +0800

    检测浏览器主题；移除无用页；添加页面文件；

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,24 +1,21 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)') // 是深色
+  if (isDarkTheme.matches) {
+    // 是深色
+    // 主题设置为深色。
+  } else {
+    // 不是深色
+    // 主题设置为浅色。
+  }
+})
+</script>
 
 <style scoped>
 header {
