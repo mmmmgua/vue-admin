3684cb50f63e35f3fcf91efc2882bf904dff6e4c
mmmmgua <542126316@qq.com>
2023-08-07 05:48:08 +0800

    添加pinia插件持久化

--- vue-admin/src/stores/plugins/sotrage-plugin.js
+++ vue-admin/src/stores/plugins/sotrage-plugin.js
@@ -1,17 +1,25 @@
 // pinia plugin https://pinia.vuejs.org/zh/core-concepts/plugins.html
-import { setStorage, removeStorage } from '../../utils/storage'
+import { setStorage, removeStorage, getStorage } from '../../utils/storage'
 
 // 插件4参：app, options, pinia, store
 export function storagePlugin({ options, store }) {
   const persistMap = options.persist
-  for (const key in persistMap) {
-    persistMap[key].key === 'localStorage'
-    return store.$subscribe(({ events }) => {
-      if (events.newValue) {
-        setStorage(events.key, events.newValue)
+  if (!persistMap) return
+  store.$subscribe((mutation) => {
+    Object.keys(persistMap).forEach((key) => {
+      if (key !== mutation.events.key) return
+      if (mutation.events.newValue) {
+        setStorage(key, mutation.events.newValue)
       } else {
-        removeStorage(events.key)
+        removeStorage(key)
       }
     })
-  }
+  })
+
+  store.$patch((state) => {
+    Object.keys(persistMap).forEach((key) => {
+      const storage = getStorage(key)
+      state[key] = storage ?? state[key]
+    })
+  })
 }
