c71d7f90a2bbb6b123e8e2f1357bdae1645d3cc8
mmmmgua <542126316@qq.com>
2023-08-16 09:52:15 +0800

    修复侧边菜单栏选中展开问题

--- vue-admin/src/layout/SlideBar/SlideBar.vue
+++ vue-admin/src/layout/SlideBar/SlideBar.vue
@@ -1,50 +1,49 @@
 <template>
-  <a-layout-sider :collapsed="isMenuShown" :trigger="null" collapsible :theme="appStore.theme">
+  <a-layout-sider :collapsed="props.isMenuShown" :trigger="null" collapsible :theme="appStore.theme">
     <div class="logo"></div>
-    <a-menu v-model:selectedKeys="selectedKeys" :open-keys="selectedKeys" :theme="appStore.theme" mode="inline" :items="menuItems"
-      @click="routerLink"></a-menu>
+    <a-menu v-model:selectedKeys="selectedKeys" v-model:openKeys="openKeys" :theme="appStore.theme" mode="inline"
+      :items="menuItems" @click="routerLink"></a-menu>
   </a-layout-sider>
 </template>
 
 <script setup lang="ts">
-import { computed, inject, onMounted, ref } from 'vue'
+import { inject, onMounted, ref } from 'vue'
 import { RouteRecordRaw, useRouter } from 'vue-router'
 import { usePermissionStore } from '@/stores/permission'
 import { useAppStore } from '@/stores/app'
+import { ItemType } from 'ant-design-vue/es/menu/src/hooks/useItems'
 
 const t: any = inject('t')
 const router = useRouter()
 const props = defineProps(['isMenuShown'])
 
-const collapsed = ref(false)
 const appStore = useAppStore()
 const permission_routes = usePermissionStore().permission_routes
-const selectedKeys = ref([])
-const menuItems = ref([])
+const openKeys  = ref<string[]>([])
+const selectedKeys = ref<string[]>([])
+const menuItems = ref<ItemType[]>([])
 
 onMounted(() => {
   menuItems.value = generateSlideBarMenu(permission_routes)
   setActiveMenu(menuItems.value)
 })
 
-function generateSlideBarMenu(routes: Array<RouteRecordRaw>) {
-  const res: Array<any> = []
+function generateSlideBarMenu(routes: Array<RouteRecordRaw>): ItemType[] {
+  const res: ItemType[] = []
   routes.forEach((route) => {
     if (typeof route?.meta?.hidden !== 'boolean' || !route?.meta?.hidden) {
-      const tmp = {
-        path: route.path,
+      const tmp: ItemType = {
         label: t('menu.' + (route?.meta?.title ?? 'menu')),
         title: t('menu.' + (route?.meta?.title ?? 'menu')),
         key: route.path
       }
-      if (route.redirect && route.children) {
-        tmp.path = route.children[0].path
+      if (route.redirect && route?.children?.length) {
         tmp.label = t('menu.' + route.children[0]?.meta?.title ?? 'menu')
         tmp.title = t('menu.' + route.children[0]?.meta?.title ?? 'menu')
         tmp.key = route.children[0].path
       } else {
         if (route.children && route.children.length > 0) {
-          tmp['children'] = generateSlideBarMenu(route.children)
+          tmp.children = generateSlideBarMenu(route.children)
         }
       }
       res.push(tmp)
@@ -54,18 +53,22 @@ function generateSlideBarMenu(routes: Array<RouteRecordRaw>) {
   return res
 }
 
-function routerLink({ key }): void {
-  router.push({ path: key, replace: true })
+function routerLink(item: ItemType): void {
+  router.push({ path: item?.key?.toString(), replace: true })
 }
 
-function setActiveMenu(menus: never[]): void {
-  if (menus.length) {
+function setActiveMenu(menus: ItemType[]): void {
+  if (menus.length > 0) {
     const currentPath: string = router.currentRoute.value.path
     menus.forEach(menu => {
-      if (currentPath === menu.key) {
+      if (menu && currentPath === menu.key) {
+        // 选中目标菜单
         selectedKeys.value.push(menu.key)
+        // 展开父级菜单
+        const pathList = menu.key.split('/').filter(Boolean)
+        openKeys.value.push(`/${pathList.slice(0, pathList.length - 1).join('/')}`)
       }
-      if (menu.children && menu.children.length > 0) {
+      if (menu && menu.children && menu.children.length > 0) {
         setActiveMenu(menu.children)
       }
     })
@@ -74,13 +77,15 @@ function setActiveMenu(menus: never[]): void {
 </script>
 
 <style>
-.ant-menu-dark{ 
+.ant-menu-dark {
   background: var(--color-background);
 }
-.ant-layout .ant-layout-sider  {
+
+.ant-layout .ant-layout-sider {
   background: var(--color-background);
   box-shadow: 6px 0px 10px 1px rgba(20, 20, 20, 0.1)
 }
+
 .ant-menu-dark.ant-menu-inline .ant-menu-sub.ant-menu-inline {
   background: var(--color-background);
 }
