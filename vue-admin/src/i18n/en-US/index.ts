e9ccfe48cfb8536471150c51973c5381529f9357
mmmmgua <542126316@qq.com>
2023-08-17 11:10:06 +0800

    优化i18n

--- vue-admin/src/i18n/en-US/index.ts
+++ vue-admin/src/i18n/en-US/index.ts
@@ -1,9 +1,13 @@
 import { getFileName } from '@/utils/url'
-const localeModules = import.meta.glob('./**.json')
+const localeModules = import.meta.glob('./**.json',{eager:true})
+const viewsModules = import.meta.globEager('./views/**.json',{eager:true})
 const data = {}
 
 Object.keys(localeModules).forEach((key) => {
   data[getFileName(key)] = localeModules[key].default
+})
+Object.keys(viewsModules).forEach((key) => {
+  data[getFileName(key)] = viewsModules[key].default
 })
 // modules entry
 /** when your routing map is too long, you can split it into small modules **/
--- vue-admin/src/i18n/zh-CN/index.ts
+++ vue-admin/src/i18n/zh-CN/index.ts
@@ -1,9 +1,14 @@
 import { getFileName } from '@/utils/url'
-const localeModules = import.meta.globEager('./**.json')
+const localeModules = import.meta.glob('./**.json', {eager:true})
+const viewsModules = import.meta.glob('./views/**.json', {eager:true})
 const data = {}
 
-Object.keys(localeModules).forEach((key) => {
+Object.keys(localeModules).forEach( (key) => {
   data[getFileName(key)] = localeModules[key].default
+})
+
+Object.keys(viewsModules).forEach( (key) => {
+  data[getFileName(key)] = viewsModules[key].default
 })
 // modules entry
 /** when your routing map is too long, you can split it into small modules **/
